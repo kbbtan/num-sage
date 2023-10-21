63fa7c7d5bbd6fb0652921ce6427d6e378de2106
Kian Boon <kianboon@yctan.com>
2023-10-21 18:20:14 -0400

    Added frontend for test page and global colors

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,16 +1,16 @@
 const Navbar = () => {
   return (
-    <nav className="bg-transparent mt-3">
+    <nav className="bg-transparent pt-3 h-[13vh]">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-                <a href="#" className="text-white px-3 py-2 text-3xl font-medium">NumSage</a>
+                <a href="#" className="text-text-accent px-3 py-2 text-3xl font-medium">NumSage</a>
 
                 <div className="flex flex-1 items-stretch justify-end">
                     <div className="flex space-x-4">
-                        <a href="#" className="text-[#FFC435] rounded-md px-3 py-2 text-2xl font-medium" aria-current="page">Test</a>
-                        <a href="#" className="text-[#393E46] hover:bg-[#393E46] hover:text-[#FFC435] rounded-md px-3 py-2 text-2xl font-medium">Learn</a>
-                        <a href="#" className="text-[#393E46] hover:bg-[#393E46] hover:text-[#FFC435] rounded-md px-3 py-2 text-2xl font-medium">About</a>
-                        <a href="#" className="text-[#393E46] hover:bg-[#393E46] hover:text-[#FFC435] rounded-md px-3 py-2 text-2xl font-medium">Settings</a>
+                        <a href="#" className="text-accent rounded-md px-3 py-2 text-2xl font-medium" aria-current="page">Test</a>
+                        <a href="#" className="text-sub-color hover:bg-sub-color hover:text-accent rounded-md px-3 py-2 text-2xl font-medium">Learn</a>
+                        <a href="#" className="text-sub-color hover:bg-sub-color hover:text-accent rounded-md px-3 py-2 text-2xl font-medium">About</a>
+                        <a href="#" className="text-sub-color hover:bg-sub-color hover:text-accent rounded-md px-3 py-2 text-2xl font-medium">Settings</a>
                     </div>
                 </div>  
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "bg-color": "#222831",
+        "sub-color": "#393E46",
+        "accent": "#FFC435",
+        "text-accent": "#EEEEEE"
+      }
     },
   },
   plugins: [],
