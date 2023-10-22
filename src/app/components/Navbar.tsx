5d7b7d9787135ef3e7142590d63c06c2cfa7fa87
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-10-21 22:06:14 -0400

    create server

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,55 @@
 const Navbar = () => {
   return (
-    <nav className="bg-transparent pt-3 h-[13vh]">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-                <a href="#" className="text-text-accent px-3 py-2 text-3xl font-medium">NumSage</a>
+    <nav className="h-[13vh] bg-transparent pt-3">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <a
+            href="#"
+            className="px-3 py-2 text-3xl font-medium text-text-accent"
+          >
+            NumSage
+          </a>
 
-                <div className="flex flex-1 items-stretch justify-end">
-                    <div className="flex space-x-4">
-                        <a href="#" className="text-accent rounded-md px-3 py-2 text-2xl font-medium" aria-current="page">Test</a>
-                        <a href="#" className="text-sub-color hover:bg-sub-color hover:text-accent rounded-md px-3 py-2 text-2xl font-medium">Learn</a>
-                        <a href="#" className="text-sub-color hover:bg-sub-color hover:text-accent rounded-md px-3 py-2 text-2xl font-medium">About</a>
-                        <a href="#" className="text-sub-color hover:bg-sub-color hover:text-accent rounded-md px-3 py-2 text-2xl font-medium">Settings</a>
-                    </div>
-                </div>  
+          <div className="flex flex-1 items-stretch justify-end">
+            <div className="flex space-x-4">
+              <a
+                href="#"
+                className="rounded-md px-3 py-2 text-2xl font-medium text-sub-color hover:bg-sub-color hover:text-accent"
+                aria-current="page"
+              >
+                Create
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-2 text-2xl font-medium text-sub-color hover:bg-sub-color hover:text-accent"
+                aria-current="page"
+              >
+                Join
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-2 text-2xl font-medium text-sub-color hover:bg-sub-color hover:text-accent"
+              >
+                Learn
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-2 text-2xl font-medium text-sub-color hover:bg-sub-color hover:text-accent"
+              >
+                About
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-2 text-2xl font-medium text-sub-color hover:bg-sub-color hover:text-accent"
+              >
+                Settings
+              </a>
             </div>
+          </div>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
