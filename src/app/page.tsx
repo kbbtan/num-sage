c0874f1f5c9c03bd40551373af73a57c462a3258
Kian Boon <kianboon@yctan.com>
2023-10-22 00:47:14 -0400

    Added results page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import * as Colyseus from "colyseus.js";
 import LanguageMenu from "./components/LanguageMenu";
 import { numToString } from "@/utils/numberConverter";
 
+import Results from "./components/Results";
+
 const DEFAULT_LANGUAGE = "English";
 const DEFAULT_LOCALE = "en";
 const DEFAULT_TIMEOUT = 60;
@@ -99,48 +101,7 @@ const Home = () => {
   return (
     <div className="flex h-[87vh] w-full items-center justify-center">
       <div className="flex flex-col items-center w-3/4">
-        <div className="relative w-full text-center">
-          {timer ? (
-            <>
-              <h1 className="text-green-900 text-3xl">{counter}</h1>
-              <h1 className="text-sub-color text-5xl">{seconds}</h1>
-            </>
-          ) : (
-            <button
-              className={`
-                        bg-transparent text-center text-3xl
-                        text-sub-color
-                        hover:cursor-pointer hover:text-text-accent
-                        ${languageMenuOpen && "text-text-accent"}
-                    `}
-              onClick={() => setLanguageMenuOpen(!languageMenuOpen)}
-            >
-              {language}
-            </button>
-          )}
-
-          {languageMenuOpen && <LanguageMenu selectOption={selectOption} />}
-        </div>
-
-        <h1
-          ref={promptRef}
-          className="my-10 text-7xl font-medium text-text-accent"
-        >
-          {prompt}
-        </h1>
-
-        <input
-          type="number"
-          ref={inputRef}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-          autoFocus
-          className="
-                    w-1/2 rounded border border-sub-color bg-transparent px-5 py-2 
-                    text-center text-2xl
-                    text-text-accent
-                "
-        />
+        <Results/>
 
         <button
           onClick={restart}
