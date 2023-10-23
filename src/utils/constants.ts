b77e46fb0e27d8a717cd977b3ea7acde2d36630d
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-10-23 01:12:55 -0400

    use Adaptable hosted domain

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_LANGUAGE = "English";
 export const DEFAULT_LOCALE = "en";
 export const DEFAULT_TIMEOUT = 60;
+export const DOMAIN = "num-sage.adaptable.app";
--- src/utils/multiplayer.ts
+++ src/utils/multiplayer.ts
@@ -1,3 +1,8 @@
 import * as Colyseus from "colyseus.js";
+import { DOMAIN } from "./constants";
 
-export const CLIENT = new Colyseus.Client("ws://localhost:2567");
+export const CLIENT = new Colyseus.Client(
+  process.env.NODE_ENV === "production"
+    ? `wss://${DOMAIN}`
+    : "ws://localhost:2567",
+);
